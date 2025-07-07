b0119e0a84e821162a6e78b0c4589ce65b082b41
NathanScarrott <nathanscarrott18@gmail.com>
2025-07-07 18:57:37 +0100

    Remove SQL stuff for now.
    Add playlist processing

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 import os
 
 from fastapi import FastAPI, Request
-from youtube_transcript_api import YouTubeTranscriptApi
 
-import pymysql
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import Playlist
 
 from dotenv import load_dotenv
 
@@ -13,36 +13,50 @@ load_dotenv()
 
 app = FastAPI()
 
-conn = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='password',
-    db='mydatabase',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
-
-@app.get("/captions/{video_id}")
-def get_transcript(video_id):
-    api_key = os.getenv("API_KEY")
-    
-    list_url = f"https://www.googleapis.com/youtube/v3/captions?part=snippet&videoId={video_id}&key={api_key}"
-    response = requests.get(list_url)
-    
-    if response.status_code == 200:
-        caption_data = response.json()
-        return caption_data
-    else:
-        return {"error": f"Failed to fetch captions: {response.status_code}", "details": response.json()}
-    
-
 @app.get("/transcript/{video_id}")
-def get_transcript_simple(video_id:str):
+def get_video_transcript(video_id:str):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        text = " ".join([entry['text'] for entry in transcript])
+        transcript_raw = YouTubeTranscriptApi.get_transcript(video_id)
 
-        return {"transcript" : text}
+        transcript_list = [transcript["text"] for transcript in transcript_raw]
+        result = " ".join(transcript_list)
+
+        return {"transcript" : result}
     except Exception as e:
         return {"error": str(e)}
+    
+@app.get("/transcripts/{playlist_id}")
+def get_playlist_transcript(playlist_id:str):
+    playlist_ids = _get_video_ids_from_playlist(playlist_id)
+    print(playlist_ids)
+    transcripts = []
+
+    for video_id in playlist_ids:
+        try:
+            transcript_raw = YouTubeTranscriptApi.get_transcript(video_id)
+
+            transcript_list = [transcript["text"] for transcript in transcript_raw]
+            result = " ".join(transcript_list)
+
+            transcripts.append(result)
+        except Exception as e:
+            print(f"Error : {str(e)} for ID: {video_id}")
+    return {"transcripts": transcripts}
+
+    
+def _get_video_ids_from_playlist(playlist_id:str):
+    api_key = os.getenv("API_KEY")
+    
+    list_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId={playlist_id}&maxResults=50&key={api_key}"
+    response = requests.get(list_url)
+
+    response_json = response.json()
+
+    ids = [item["contentDetails"]["videoId"] for item in response_json["items"]]
+
+    if response.status_code == 200:
+        return ids
+    else:
+        return {"error": f"Failed to fetch playlist: {response.status_code}", "details": response.json()}
+    
 
