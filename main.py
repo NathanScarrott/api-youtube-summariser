74676e318b81e5b8cfc3f6372d9adcc1fdcdc995
NathanScarrott <nathanscarrott18@gmail.com>
2025-07-09 22:04:03 +0100

    Refactor transcript retrieval by creating a dedicated function to handle video ID transcripts. This improves code readability and reduces duplication in the API endpoints.

--- main.py
+++ main.py
@@ -21,15 +21,7 @@ app = FastAPI()
 @app.get("/transcript/{video_id}")
 def get_video_transcript(video_id:str):
     try:
-        transcript_raw = YouTubeTranscriptApi.get_transcript(video_id)
-
-        transcript_list = [transcript["text"] for transcript in transcript_raw]
-        result = " ".join(transcript_list)
-
-        connection = get_connection(db_name="summaries.db")
-        create_table(connection=connection)
-        insert_transcript(connection, transcript=result, videoId=video_id, playlistId = None)
-
+        result = _get_transcript_from_video_id(video_id)
         return {"transcript" : result}
     except Exception as e:
         return {"error": str(e)}
@@ -41,15 +33,7 @@ def get_playlist_transcript(playlist_id:str):
 
     for video_id in playlist_ids:
         try:
-            transcript_raw = YouTubeTranscriptApi.get_transcript(video_id)
-
-            transcript_list = [transcript["text"] for transcript in transcript_raw]
-            result = " ".join(transcript_list)
-
-            connection = get_connection(db_name="summaries.db")
-            create_table(connection=connection)
-            insert_transcript(connection, transcript=result, videoId=video_id, playlistId = playlist_id)
-
+            result = _get_transcript_from_video_id(video_id)
             transcripts.append(result)
         except Exception as e:
             print(f"Error : {str(e)} for ID: {video_id}")
@@ -119,6 +103,18 @@ def _get_video_ids_from_playlist(playlist_id:str):
         return ids
     else:
         return {"error": f"Failed to fetch playlist: {response.status_code}", "details": response.json()}
+    
+def _get_transcript_from_video_id(video_id: str):
+    transcript_raw = YouTubeTranscriptApi.get_transcript(video_id)
+
+    transcript_list = [transcript["text"] for transcript in transcript_raw]
+    result = " ".join(transcript_list)
+
+    connection = get_connection(db_name="summaries.db")
+    create_table(connection=connection)
+    insert_transcript(connection, transcript=result, videoId=video_id, playlistId = None)
+
+    return result
     
 
 
