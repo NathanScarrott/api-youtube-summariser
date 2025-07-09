09c2796b1cc00c5a0d2f1c402ae4f2b6e1f20e17
NathanScarrott <nathanscarrott18@gmail.com>
2025-07-09 21:52:50 +0100

    Added summary generation (API Endpoint and functions which call the model)
    Changed the transcript endpoints to insert in the transcript column rather than summary for correctness
    
    Refactor to standaridsed some repeated processes such as generated summary/transcript for video vs playlist since playlist just loops.

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 from pytube import Playlist
 
-from sql_action import get_connection, create_table, insert_summary, fetch_summary
+from sql_action import get_connection, create_table, insert_summary, fetch_summary, insert_transcript, fetch_summary_by_video_id
+
+from llm import model_request
 
 from dotenv import load_dotenv
 
@@ -26,7 +28,7 @@ def get_video_transcript(video_id:str):
 
         connection = get_connection(db_name="summaries.db")
         create_table(connection=connection)
-        insert_summary(connection, summary=result, videoId=video_id, playlistId = None)
+        insert_transcript(connection, transcript=result, videoId=video_id, playlistId = None)
 
         return {"transcript" : result}
     except Exception as e:
@@ -46,14 +48,63 @@ def get_playlist_transcript(playlist_id:str):
 
             connection = get_connection(db_name="summaries.db")
             create_table(connection=connection)
-            insert_summary(connection, summary=result, videoId=video_id, playlistId = playlist_id)
+            insert_transcript(connection, transcript=result, videoId=video_id, playlistId = playlist_id)
 
             transcripts.append(result)
         except Exception as e:
             print(f"Error : {str(e)} for ID: {video_id}")
     return {"transcripts": transcripts}
 
+@app.get("/summary/{video_id}")
+def generate_video_summary(video_id:str):
+    try:
+        summary = _generate_video_summary(video_id)
+        return {"summary": summary}
+    except Exception as e:
+        return {"Error": e}
     
+@app.get("/summaries/{playlist_id}")
+def generate_playlist_summaries(playlist_id:str):
+    try:
+        summaries = []
+        video_ids = _get_video_ids_from_playlist(playlist_id)
+        for video_id in video_ids:
+            summary = _generate_video_summary(video_id)
+
+            summaries.append(summary)
+
+            connection = get_connection(db_name="summaries.db")
+            create_table(connection=connection)
+            insert_summary(connection, summary, video_id)
+
+        return {"summaries": summaries}
+    except Exception as e:
+        return {"Error": e}
+
+def _generate_video_summary(video_id):
+    connection = get_connection(db_name="summaries.db")
+    create_table(connection=connection)
+
+    rows = fetch_summary_by_video_id(connection, video_id)
+    
+    if not rows:
+        return {"error": "No transcript found for this video"}
+    
+    transcript = rows[0][4]
+
+    prompt = f"""
+    Analayze the following transcript from a youtube video and generate a summary:
+
+    <transcript>
+    {transcript}
+    </transcript>
+
+    Proceed with the summary wihout any leading phrases such as "Here is a summary of ...".
+    """
+
+    summary = model_request(prompt)
+    return summary
+
 def _get_video_ids_from_playlist(playlist_id:str):
     api_key = os.getenv("API_KEY")
     
@@ -70,3 +121,6 @@ def _get_video_ids_from_playlist(playlist_id:str):
         return {"error": f"Failed to fetch playlist: {response.status_code}", "details": response.json()}
     
 
+
+                        
+    
